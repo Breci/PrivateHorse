432373ff53032143480be8bbed0b2fb2b6707b5b
Breci <Breci@Breci-PC.home>
2013-09-25 16:07:21 +0200

    permission changed : privatehorse.all to privatehorse.use permission added : private horse.damage

--- src/fr/breci/PrivateHorse/event/PrivatiseHorseEvent.java
+++ src/fr/breci/PrivateHorse/event/PrivatiseHorseEvent.java
@@ -21,25 +21,25 @@ public class PrivatiseHorseEvent implements Listener{
 			Player p = e.getPlayer();
 			AnimalTamer at = p;
 			Entity entity  = e.getRightClicked();
-			if (entity instanceof Horse){ //si on click sur un cheval
+			if (entity instanceof Horse){ //si on clique sur un cheval
 				Horse horse = (Horse) entity;
 				Material m = p.getItemInHand().getType();
-				if(p.hasPermission("privatehorse.all") && 
+				if(p.hasPermission("privatehorse.use") && 
 						horse.getOwner() != at ){ //Si le joueur n'est pas le propriétaire
 					if (m == Material.NAME_TAG){ //si le joueur utilise un nametag
-						if (horse.getOwner() != null){ //si le cheval est sauvage et n'a pas de propriétaire.
+						if (horse.getOwner() != null){ //si le cheval est sauvage et a un propriétaire.
 							p.sendMessage("Ce cheval a deja un proprietaire!");
 							e.setCancelled(true);
 						}
 						else{ //On apprivoise ce cheval
-							if (p.hasPermission("privatehorse.all")
+							if (p.hasPermission("privatehorse.use")
 									&& !horse.isTamed())
 								horse.setTamed(true);
 							horse.setOwner(p);
 							p.sendMessage("Vous venez de privatiser ce cheval !");
 						}
 					}
-					if (p.hasPermission("privatehorse.all") && 
+					if (p.hasPermission("privatehorse.use") && 
 							horse.getOwner() !=  null && horse.getOwner() != p){
 						p.sendMessage("Ce cheval a deja un proprietaire!");
 						Location l = p.getLocation();
